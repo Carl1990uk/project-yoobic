02a5287abed7079318a4ed36f5fb7d87edef7ae0
Carl Clarke <carlclarke1990@me.com>
2022-06-27 09:37:00 +0200

    <feat>:<movie details page>

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'tab3',
     loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
+  },
+  {
+    path: 'movie-details',
+    loadChildren: () => import('./movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
   }
 ];
 
