a65817bfc749dc1a8723e074c31a1f9a0e4cafca
Carl Clarke <carlclarke1990@me.com>
2022-06-27 09:45:55 +0200

    <feat><chat page + app routing>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,50 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import{ redirectUnauthorizedTo, redirectLoggedInTo, canActivate,} from '@angular/fire/auth-guard';
+import { identity } from 'rxjs';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    loadChildren: () => 
+      import('./login/login.module').then( m => m.LoginPageModule),
+      //if logged in already, you will be sent to home page 
+    ...canActivate(redirectLoggedInToHome)
   },
   {
-    path: '',
-    redirectTo: 'home',
+    path: 'home',
+    loadChildren: () => 
+      import('./home/home.module').then( m => m.HomePageModule),
+      //if not logged in: you will be sent to login page
+    ...canActivate(redirectUnauthorizedToLogin)
+    
+  },
+  {
+    path: 'home/:id',
+    loadChildren: () => import('./home/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
+  },
+  {
+    path: 'tabs',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    path: 'tab1',
+    loadChildren: () => import('./home/tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./home/tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'tab3',
+    loadChildren: () => import('./home/tab3/tab3.module').then( m => m.Tab3PageModule)
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
   },
 ];
 
