6d4e72c8056fa2a2505fce1def19b645c2d4dd6d
Carl Clarke <carlclarke1990@me.com>
2022-06-27 09:31:49 +0200

    <build><ionic labs, firebase>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'yoobic-project',
+    appId: '1:1077389455943:web:7e127e6a98c1a190edb762',
+    storageBucket: 'yoobic-project.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyCHdbkHUQ9CWy5brkj8bNkBWTpRYvZmo9A',
+    authDomain: 'yoobic-project.firebaseapp.com',
+    messagingSenderId: '1077389455943',
+    measurementId: 'G-9ZPP28JTQR',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'yoobic-project',
+    appId: '1:1077389455943:web:7e127e6a98c1a190edb762',
+    storageBucket: 'yoobic-project.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyCHdbkHUQ9CWy5brkj8bNkBWTpRYvZmo9A',
+    authDomain: 'yoobic-project.firebaseapp.com',
+    messagingSenderId: '1077389455943',
+    measurementId: 'G-9ZPP28JTQR',
+  },
   production: false
 };
 
